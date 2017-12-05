fe77e7bbc33ad1207a564d135b9eac2a08e977ff
Carlos Rodrigues <carlos.rodrigues@ev-box.com>
2017-12-05 14:58:11 +0100

    setting coors origin pre-flight

--- src/app.js
+++ src/app.js
@@ -34,6 +34,7 @@ function createApp() {
   };
   logger.info('Using CORS options:', corsOpts);
   app.use(cors(corsOpts));
+  app.options('*', cors())
 
   // Limit to 10mb if HTML has e.g. inline images
   app.use(bodyParser.text({ limit: '4mb', type: 'text/html' }));
